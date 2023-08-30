c9ee53e4f6144d91ca697405c913722b0024f1d7
quentin.medina <quentin.medina@sii.fr>
2023-08-30 10:57:32 +0200

    Update the package and example to make it great again

--- examples/js/index.js
+++ examples/js/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const appRoot = process.env.PWD;
+const cors = require('cors');
 const app = express()
-app.get('/', function (req, res) {
+app.use(cors())
+app.get('/test', function (req, res) {
     res.sendFile(__dirname + '/index.html');
+    console.log(`${appRoot}/dist/bundle.js`)
 });
 app.get('/ottm-modal-feedback.js', function(req, res) {
-    res.sendFile(__dirname + '/node_modules/@ottm/survey-front-modal/dist/bundle.js');
+    res.sendFile(`${appRoot}/dist/bundle.js`);
 });
 app.listen(4000, function () {
     console.log("Example app listening at http://localhost:4000")
